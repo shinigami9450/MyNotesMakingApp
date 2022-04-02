b63863f5087c5b0626bddaa01d4b86d27449d2c0
Aman Singh <aman.yaduvanshi.560@gmail.com>
2022-04-02 15:53:44 +0530

    Notes making app....

---
+++ app/src/main/java/com/example/notes/Note.kt
@@ -0,0 +1,9 @@
+package com.example.notes
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes_table")
+class Note(val text:String) {
+    @PrimaryKey(autoGenerate = true)var id=0
+}
